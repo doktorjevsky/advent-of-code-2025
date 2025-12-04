7a90967d7218595176ce8258a1db5ae7636b2061
Simon Porsgaard-EXT <simon.porsgaard@gotids20p.jeppesensystems.com>
2025-12-04 13:07:04 +0100

    day4

---
+++ day4/day4.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+[ -z "$INPUT" ] && INPUT=${SCRIPTDIR}/input.txt
+
+. ${SCRIPTDIR}/../utils/iterate.sh
+. ${SCRIPTDIR}/../utils/nicetohave.sh
+. ${SCRIPTDIR}/../utils/vec2d.sh
+
+
+
+get_neighbors()
+{
+    local grid=$1
+    local row="$2"
+    local col="$3"
+    local i j 
+
+    for ((i=-1; i < 2; i++)); do 
+        for ((j=-1; j < 2; j++)); do 
+            if (( i != 0 || j != 0 )) && in_bounds $((row + i)) $((col + j)) $ROWS $COLS ; then
+                gr_get $grid $((row + i)) $((col + j))
+            
+            fi
+        done 
+    done 
+}
+
+set_grid() {
+    local -n gridname="$1"
+    mapfile "$1" < "$INPUT"
+
+    ROWS=${#gridname}
+    COLS=$(echo ${gridname[0]} | wc -m)
+    ((COLS--))
+    ((ROWS--))
+}
+
+count_char() {
+    local str="$1"
+    local char="$2"
+    local tmp="${str//"$char"/}"
+    echo $(( ${#str} - ${#tmp} ))
+}
+
+iterate()
+{
+    local i
+    for ((i=0; i < ROWS; i++)); do 
+        (
+        local j
+        local I=$i
+        for ((j=0; j < COLS; j++)); do 
+            if [[ $(gr_get GRID $I $j) == '@' ]]; then 
+            
+                local nbors
+                nbors=$(get_neighbors GRID $I $j)
+                if (( $(count_char $nbors '@') < 4 )); then 
+                    echo $I $j
+                fi
+            
+            fi 
+        done 
+        ) &
+        (( $(jobs -r | wc -l) >= $(nproc) )) && wait -n
+    done
+
+    wait
+
+}
+
+part_2()
+{
+    local c y x old_row
+    while true; do
+        c=0
+        while read -r y x; do 
+            old_row=${GRID[$y]}
+            GRID[$y]="${old_row:0:$x}.${old_row:$((x+1))}"
+            ((c++))
+        done < <(iterate)
+        ((c == 0)) && break
+        echo $c
+    done | sum | prompt "PART 2: "
+     
+}
+
+part_1()
+{
+    iterate | wc -l | prompt "PART 1: "
+}
+
+main() (
+    set_grid GRID
+    part_1
+    part_2
+    
+)
+
+(return 0 2>/dev/null) || time main "$@"
